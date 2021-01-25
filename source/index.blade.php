cd981d1e5a53184c667a513f26dca6b768633158
kayanhales <daybreakbliss@gmail.com>
2021-01-25 17:47:49 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,16 +3,14 @@
 @section('content')
     
 
-    <p><a href="https://ibb.co/T1rTNdx"><img src="https://i.ibb.co/xY3srdt/40thweddinganniversary-With-NOTHING.png" alt="40thweddinganniversary-With-NOTHING" border="0"></a></p>
+    <p><center><a href="https://ibb.co/T1rTNdx"><img src="https://i.ibb.co/xY3srdt/40thweddinganniversary-With-NOTHING.png" alt="40thweddinganniversary-With-NOTHING" border="0"></a></center></p>
     <h1>YouTube</h1>
     <p>Coming soon</p>
     <h1>Zoom</h1>
-    <p>Join Zoom Meeting
-https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09
+    <p>Join Zoom Meeting: <a href="https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09">https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09</a></p>
 
-Meeting ID: 823 7257 6873
-Passcode: 511173
-One tap mobile
-+19292056099,,82372576873#,,,,*511173# US (New York)</p>
+<p>Meeting ID: 823 7257 6873</p>
+<p>Passcode: 511173</p>
+<p>One tap mobile: +19292056099,,82372576873#,,,,*511173# US (New York)</p>
     
 @endsection
