e5298ff2d1f4e82c782670dc020d6ab8a131726f
kayanhales <daybreakbliss@gmail.com>
2021-01-25 17:52:42 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,10 +7,9 @@
     <h1>YouTube</h1>
     <p>Coming soon</p>
     <h1>Zoom</h1>
-    <p>Join Zoom Meeting: <a href="https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09">https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09</a></p>
+    <p>Join with the Zoom App: <a href="https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09">https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09</a></p>
 
-<p>Meeting ID: 823 7257 6873</p>
-<p>Passcode: 511173</p>
-<p>One tap mobile: +19292056099,,82372576873#,,,,*511173# US (New York)</p>
+<p>Meeting ID: 823 7257 6873, Passcode: 511173</p>
+<p>Or Dial-in: +19292056099, Meeting ID: 82372576873#, Passcode: *511173#</p>
     
 @endsection
