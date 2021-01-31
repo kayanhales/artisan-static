bc328873e93a2c4671250238ac2d9c1f2da76380
kayanhales <daybreakbliss@gmail.com>
2021-01-30 20:33:21 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,8 +8,10 @@
     <p><a href="https://youtu.be/BfX3-uG_Kao">https://youtu.be/BfX3-uG_Kao</a></p>
     <h1>Zoom</h1>
     <p>Join with the Zoom App: <a href="https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09">https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09</a></p>
-    <h1>Program - 4PM to 6PM EST</h1>
-    <p>
+    <p>Meeting ID: 823 7257 6873, Passcode: 511173</p>
+    <p>Or Dial-in: +19292056099, Meeting ID: 82372576873#, Passcode: *511173#</p>
+    <h1>Program</h1>
+    <p><small> Start time: 4PP. End Time: 6PM</small>
         <ol>
             <li>Arrival of Guests
             <li>Welcome Guests by the MC - Bro Michael</li>
@@ -22,7 +24,5 @@
             <li>Thank you message from the celebrants</li>
         <ol>
     </p>
-<p>Meeting ID: 823 7257 6873, Passcode: 511173</p>
-<p>Or Dial-in: +19292056099, Meeting ID: 82372576873#, Passcode: *511173#</p>
     
 @endsection
