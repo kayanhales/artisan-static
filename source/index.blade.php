a120e18a8623749a0fc992aa1482304854212ff0
kayanhales <daybreakbliss@gmail.com>
2021-01-30 20:31:34 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,16 +9,19 @@
     <h1>Zoom</h1>
     <p>Join with the Zoom App: <a href="https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09">https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09</a></p>
     <h1>Program - 4PM to 6PM EST</h1>
-    <p><ol><li>Arrival of Guests
-<li>Welcome Guests by the MC - Bro Michael</li>
-<li>Entrance of Celebrants</li>
-<li>Devotion</li>
-<li>Welcome remarks by the MC</li>
-<li>Love Story of the Celebrants - Kayan Hales Clarke</li>
-<li>Cake cutting</li>
-<li>Dinner/Toasts</li>
-<li>Thank you message from the celebrants</li>
-    <ol></p>
+    <p>
+        <ol>
+            <li>Arrival of Guests
+            <li>Welcome Guests by the MC - Bro Michael</li>
+            <li>Entrance of Celebrants</li>
+            <li>Devotion</li>
+            <li>Welcome remarks by the MC</li>
+            <li>Love Story of the Celebrants - Kayan Hales Clarke</li>
+            <li>Cake cutting</li>
+            <li>Dinner/Toasts</li>
+            <li>Thank you message from the celebrants</li>
+        <ol>
+    </p>
 <p>Meeting ID: 823 7257 6873, Passcode: 511173</p>
 <p>Or Dial-in: +19292056099, Meeting ID: 82372576873#, Passcode: *511173#</p>
     
